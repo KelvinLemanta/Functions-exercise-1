dc1222528b43550c3cb9307582acd4205c6167ad
KelvinLemanta <85560626+KelvinLemanta@users.noreply.github.com>
2021-12-01 14:02:00 +0300

    Add files via upload

---
+++ for loop exercise 1.cpp
@@ -0,0 +1,21 @@
+
+//137533 Kelvin Lemanta
+
+// For loop to output numbers: 12,14,16,18,20,22,24,26,28.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int counter=12;
+	for ( int counter = 12; counter <= 28; counter+2);
+	{
+		cout << " input first integer.";
+		cin >> counter;
+		cout << " the inegers are: ";
+	}
+	
+	return 0;
+}
+	
+	
